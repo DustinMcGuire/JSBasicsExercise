ef33225bada520e6c78ac37d5ef0a4adf2b01137
Dustin McGuire <dustinmcguire@KCCCs-MBP-3.lan1>
2021-02-22 14:20:03 -0500

    Completed all but 2

---
+++ jsBasicsExercise.js
@@ -0,0 +1,42 @@
+// #1
+console.log(7);
+
+// #2
+// numbers, strings, Booleans, null, undefined 
+
+// #3
+// + , - , * , / , ** , %
+
+// #4
+console.log(5%4);
+console.log(5%2);
+
+// #5
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+// #6
+console.log("Wanda" + "Vision");
+
+// #7
+console.log("Dustin" .length + "McGuire" .length);
+
+// #8
+console.log("Avengers: Endgame"[16]);
+
+// Bonus #1
+console.log("\\");
+// #2
+console.log(5**4-9);
+// #3
+
+// 4
+console.log("CAP"[1]);
+console.log("VISION"[0])
+console.log("HAWKEYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+// #5
+
